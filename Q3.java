a56c1e43b64b315279e7ddd2fb14b67db74a6b88
naveengr2001 <115767235+naveengr2001@users.noreply.github.com>
2023-03-27 00:30:47 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,14 @@
+public class Q3 {
+    public Q3() {
+    }
+
+    public static void main(String[] var0) {
+        byte var1 = 3;
+        multiply(var1, 2);
+    }
+
+    private static void multiply(int var0, int var1) {
+        var1 *= var2;
+        System.out.println(var1 + " " + var2);
+    }
+}
